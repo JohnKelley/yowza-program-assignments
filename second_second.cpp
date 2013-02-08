dce017ac81816e2a40a57e2ae71fb0cc3fea95cb
John Kelley <jkelley81@gmail.com>
2013-02-07 17:17:24 -0700

    I am not sure what is going with this problem. I have tried to manipulate the code as much as possible but no dice.

---
+++ second_second.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+//Write program to compute hours taken and hours*price per hour
+//U for undergraduate is $380 per hour and G for graduate is $395 per hour
+
+double int hours
+const char G,U
+const G,U
+
+cout << "Are you taking undergraduate classes or graduate classes? Type either "U" for undergraduate or "G" for graduate"endl;
+
+cin >> 
+
+cout << "How many hours are you currently taking?";endl
+;cin >> hours 
+
+;cout <<"You owe:" <<if U (U * 380) or if G  (G * 395)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
