c0f2fda5f6c093e2d019e842d42ba5dd96ec16ef
John Kelley <jkelley81@gmail.com>
2013-02-07 17:11:00 -0700

    I changed the whole original problem because at the time I did not understand how to create a new program.

--- third.cpp
+++ third.cpp
@@ -5,23 +5,23 @@ using namespace std;
 
 int main()
 {
- //Use waistsize and weight to compute necksize(3 x weight divided by waistsize)// Use waistsize and weight to compute hatsize(necksize divided by 2 and 1/8)
-// Use waistsize an weight to compute shoesize(50 x waistsize divided by weight)
+ //4 cents for nuts and 6 cents for bolts
+// Ask user for the number of bolts and nuts in their purchase and then calculate and print out total cost
+//Have program check order and if there are more bolts than nuts have program output "Check the order!"  Otherwise, have program output "Order is OK".
 
  
-double waistsize ;
-double weight ;
-double necksize = 3 *  weight /  waistsize ;
+double nuts, bolts;
 
-cout << "What is your weight?";
-cin >> weight;
-cout << "What is your waist size?";
-cin >> waistsize;
+cout << "How many bolts are in your order?";
+cin >> bolts;
+cout << "How many nuts are in your order?";
+cin >> nuts;
+
+if (bolts > nuts) cout << "Check the order!" << endl;
+if (nuts > bolts) cout << "Order is OK." << endl;
+cout << " Total Cost:" << (nuts * .04) + (bolts * .06) << endl;
 
 
-cout << "Necksize is: " << 3 * weight/waistsize << endl;
-cout << "Hatsize is: " <<( 3*weight/waistsize) / 2.125 << endl;
-cout << "Shoesize is: " << (50 * waistsize / weight) << endl;
 
 
 
